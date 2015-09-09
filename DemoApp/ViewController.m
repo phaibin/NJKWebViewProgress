b410abc14007b9b1d699daa14192456099d90125
Leon <phaibin@gmail.com>
2015-09-09 13:57:23 +0800

    change progress to image

--- DemoApp/ViewController.m
+++ DemoApp/ViewController.m
@@ -24,11 +24,14 @@
     _progressProxy.webViewProxyDelegate = self;
     _progressProxy.progressDelegate = self;
 
-    CGFloat progressBarHeight = 2.f;
+    CGFloat progressBarHeight = 17.f;
     CGRect navigaitonBarBounds = self.navigationController.navigationBar.bounds;
-    CGRect barFrame = CGRectMake(0, navigaitonBarBounds.size.height - progressBarHeight, navigaitonBarBounds.size.width, progressBarHeight);
+    CGRect barFrame = CGRectMake(0, navigaitonBarBounds.size.height - (NSInteger)(progressBarHeight/2), navigaitonBarBounds.size.width, progressBarHeight);
     _progressView = [[NJKWebViewProgressView alloc] initWithFrame:barFrame];
     _progressView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+    _progressView.progressBarView.backgroundColor = [UIColor clearColor];
+    _progressView.progressBarView.image = [UIImage imageNamed:@"web-loading-bar"];
+    _progressView.hidden = YES;
 
     [self loadGoogle];
 }
@@ -60,13 +63,16 @@
 
 -(void)loadGoogle
 {
-    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://google.com/"]];
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.baidu.com/"]];
     [_webView loadRequest:req];
 }
 
 #pragma mark - NJKWebViewProgressDelegate
 -(void)webViewProgress:(NJKWebViewProgress *)webViewProgress updateProgress:(float)progress
 {
+    if (progress > 0) {
+        _progressView.hidden = NO;
+    }
     [_progressView setProgress:progress animated:YES];
     self.title = [_webView stringByEvaluatingJavaScriptFromString:@"document.title"];
 }
